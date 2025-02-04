ec4bfa1e8d97f68eb18fabb3c6f9733abb323854
MichaelLELU <lelu.michael@gmail.com>
2025-02-04 12:17:06 +0100

    feat start architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-import "./App.css";
+import { Outlet } from "react-router-dom";
+import NavBar from "./compo/NavBar";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Landing from "./page/Landing";
+import About from "./page/About";
+import Contact from "./page/Contact";
+import Works from "./page/Works";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/*",
+  },
+  {
+    element: <Landing />,
+    path: "/",
+  },
+  {
+    element: <App />,
+    children: [
+      {
+        element: <About />,
+        path: "/about",
+      },
+      {
+        element: <Contact />,
+        path: "/contact",
+      },
+      {
+        element: <Works />,
+        path: "/works",
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
